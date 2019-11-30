758fbfd739c26133a4d48037e6457022efd7b7df
unknown <oskarjorgen@gmail.com>
2019-11-30 15:53:15 +0000

    Main menu scene created with controller map and a placeholder settings screen

---
+++ Horology/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("PlayGame called");
+        // Load Game scene
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame called");
+        Application.Quit();
+    }
+}
