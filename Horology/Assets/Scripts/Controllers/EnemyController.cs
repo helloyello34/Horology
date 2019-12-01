5d11c362176cffc79bf526ee41de5fcd1c6b31c1
Kristinn <kristinn34@gmail.com>
2019-12-01 13:17:30 +0000

    Enemy follows and shoots

--- Horology/Assets/Scripts/Controllers/EnemyController.cs
+++ Horology/Assets/Scripts/Controllers/EnemyController.cs
@@ -8,14 +8,19 @@ public class EnemyController : MonoBehaviour
 
     public float lookRadius = 4.0f;
     public float speed = 10f;
+    float timeSinceShot = 0;
+    public float shootInterval;
 
     Transform target;
     Rigidbody2D rb;
+
+    EnemyWeapon weapon;
     // Start is called before the first frame update
     void Start()
     {
         target = PlayerManager.instance.player.transform;
         rb = GetComponent<Rigidbody2D>();
+        weapon = GetComponentInChildren<EnemyWeapon>();
         //agent = GetComponent<NavMeshAgent>();
 
     }
@@ -23,10 +28,19 @@ public class EnemyController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        timeSinceShot += Time.fixedDeltaTime;
         float distance = Vector3.Distance(target.position, transform.position);
         if (distance <= lookRadius)
         {
-            transform.position += speed * Time.fixedDeltaTime * (target.position - transform.position).normalized;
+            float step = Time.fixedDeltaTime * speed;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            
+            if (timeSinceShot >= shootInterval)
+            {
+                timeSinceShot = 0;
+                Debug.Log("Shoot");
+                weapon.Shoot();
+            }
         }
     }
 
--- Horology/Assets/Scripts/bullet.cs
+++ Horology/Assets/Scripts/bullet.cs
@@ -2,30 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bullet : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
 
     public float speed = 40f;
     public Rigidbody2D rb;
-    public GameObject impactEffect;
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = transform.right * speed;
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Destroy(gameObject);
-
-        Enemy enemy = collision.GetComponent<Enemy>();
-
-        if (enemy)
-        {
-            enemy.Hit(40);
-        }
-
-
-    }
-
 }
