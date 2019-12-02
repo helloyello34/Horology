74f700abf1c82c9a173beea26ee0fa5e72c616eb
unknown <oskarjorgen@gmail.com>
2019-12-02 14:50:38 +0000

    Mouse input on main menu ingored

---
+++ Horology/Assets/Scripts/MouseIntercept.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseIntercept : MonoBehaviour
+{
+    private GameObject currentObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentObject = EventSystem.current.currentSelectedGameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(currentObject);
+        }
+        currentObject = EventSystem.current.currentSelectedGameObject;
+    }
+}
