a3c96f9d10ddbbd318383beda47ffceaf06bf455
Kristinn <kristinn34@gmail.com>
2019-12-02 14:29:28 +0000

    tidying up scripts

--- Horology/Assets/Scripts/Player/PlayerShootingMechanism.cs
+++ Horology/Assets/Scripts/Player/PlayerShootingMechanism.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerShootingMechanism : MonoBehaviour
 {
     Vector3 looking = new Vector3();
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
     // Update is called once per frame
     void Update()
     {
@@ -12,5 +15,17 @@ public class PlayerShootingMechanism : MonoBehaviour
         looking.y = Input.GetAxis("ShootVertical");
         float angle = Mathf.Atan2(looking.y, looking.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, -angle);
+        Debug.Log(transform.rotation);
+
+        if (Input.GetButtonDown("Fire"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        // Shoot projectile
+        Instantiate(bulletPrefab, firePoint.position, transform.rotation);
     }
 }
