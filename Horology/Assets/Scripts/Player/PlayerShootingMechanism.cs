136e87422e759680e1e64b6cea127d03580790a0
Kristinn <kristinn34@gmail.com>
2019-12-02 17:31:47 +0000

    Basic sprite for player and rotation on hand

--- Horology/Assets/Scripts/Player/PlayerShootingMechanism.cs
+++ Horology/Assets/Scripts/Player/PlayerShootingMechanism.cs
@@ -5,27 +5,43 @@ using UnityEngine;
 public class PlayerShootingMechanism : MonoBehaviour
 {
     Vector3 looking = new Vector3();
-
+    float timeSinceShot = 0;
+    
+    public float shootInterval = 0.5f;
     public Transform firePoint;
     public GameObject bulletPrefab;
+
+    
     // Update is called once per frame
     void Update()
     {
+        timeSinceShot += Time.deltaTime;
+        // Get the R axis on the joystick
         looking.x = Input.GetAxis("ShootHorizontal");
         looking.y = Input.GetAxis("ShootVertical");
+
+        // Get the angle of the joystick and rotating the object on that angle
         float angle = Mathf.Atan2(looking.y, looking.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, -angle);
         Debug.Log(transform.rotation);
 
-        if (Input.GetButtonDown("Fire"))
+        // If R1 is pushed
+        if (Input.GetButton("Fire"))
         {
-            Shoot();
+            if(timeSinceShot >= shootInterval)
+            {
+                Shoot();
+            }
         }
     }
 
+    // Shoot projectile
     void Shoot()
     {
-        // Shoot projectile
+        // Create an instance of a bullet
         Instantiate(bulletPrefab, firePoint.position, transform.rotation);
+
+        // Reset timer
+        timeSinceShot = 0f;
     }
 }
