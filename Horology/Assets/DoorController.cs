1fdeaf6cb63c381d78b9f3c3c25c365ee5302f64
Kristinn <kristinn34@gmail.com>
2019-12-03 16:33:36 +0000

    Mergeing

--- Horology/Assets/DoorController.cs
+++ Horology/Assets/DoorController.cs
@@ -16,6 +16,7 @@ public class DoorController : MonoBehaviour
     {
         if (EnemyManager.instance.FloorIsEmpty())
         {
+            Debug.Log("Disabling the doors");
             for (int i = 0; i < doorObject.transform.childCount; i++)
             {
                 if (doorObject.transform.GetChild(i).CompareTag("ClosedDoor"))
@@ -26,6 +27,7 @@ public class DoorController : MonoBehaviour
         }
         else
         {
+            Debug.Log("Closing The Doors");
             for (int i = 0; i < doorObject.transform.childCount; i++)
             {
                 if (doorObject.transform.GetChild(i).CompareTag("ClosedDoor"))
