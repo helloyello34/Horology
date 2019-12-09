3dac018c419b13c629863d97e54502fd63b5aa46
unknown <kristinn34@gmail.com>
2019-12-09 16:25:12 +0000

    Sup

---
+++ Horology/Assets/Laser.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    private LineRenderer lr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit;
+
+        hit = Physics2D.Raycast(transform.position, transform.right);
+
+        Debug.DrawRay(transform.position, transform.right, Color.gray);
+
+
+        if (hit.collider)
+        {
+            Debug.Log(hit.distance);
+            lr.SetPosition(1, new Vector3(hit.distance, 0, 0));
+        }
+
+        //if (Physics.Raycast(transform.position, transform.right, out hit)) 
+        //{
+        //    Debug.Log(hit.distance);
+        //    //if (hit.collider)
+        //    //{
+        //    //    lr.SetPosition(1, new Vector3(0, 0, hit.distance));
+        //    //}
+        //}
+    }
+}
