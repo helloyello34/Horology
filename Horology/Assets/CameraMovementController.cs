38ccc3e9aef6e30b1f39fb1c03099c2f6db0c8bf
unknown <baldur17@ru.is>
2019-12-12 11:34:19 +0000

    effects

--- Horology/Assets/CameraMovementController.cs
+++ Horology/Assets/CameraMovementController.cs
@@ -6,12 +6,12 @@ public class CameraMovementController : MonoBehaviour
 {
     
     // The object the camera follows
-    public GameObject gameObject;
+    public GameObject myGameObject;
 
     // Update is called once per frame
     void Update()
     {
         // Follow 20% of the way to the player
-        transform.position += (gameObject.transform.position - new Vector3(transform.position.x, transform.position.y, gameObject.transform.position.z)) * 0.1f;
+        transform.position += (myGameObject.transform.position - new Vector3(transform.position.x, transform.position.y, myGameObject.transform.position.z)) * 0.1f;
     }
 }
--- Horology/Assets/HitEffectController.cs
+++ Horology/Assets/HitEffectController.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class HitEffectController : MonoBehaviour
 {
     public AudioSource hitSound;
-    // Start is called before the first frame update
+
     void Start()
     {
-        Destroy(gameObject, 0.5f);
+        Destroy(gameObject, 0.2f);
     }
-
 }
