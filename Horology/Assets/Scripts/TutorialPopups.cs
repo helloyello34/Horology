8cba0f1ffcddc8e66f8d9db403ba61bfbea5f11c
unknown <oskarjorgen@gmail.com>
2019-12-13 10:06:59 +0000

    tutorial

---
+++ Horology/Assets/Scripts/TutorialPopups.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPopups : MonoBehaviour
+{
+    public List<Canvas> popUps;
+    private List<bool> isTriggered;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTriggered = new List<bool>();
+        foreach (var _ in popUps)
+        {
+            isTriggered.Add(false);
+        }
+    }
+
+    public void Trigger(int index)
+    {
+        if (popUps.Count > index && !isTriggered[index])
+        {
+            Time.timeScale = 0f;
+            popUps[index].gameObject.SetActive(true);
+            isTriggered[index] = true;
+        }
+    }
+}
