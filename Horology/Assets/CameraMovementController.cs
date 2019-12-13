85cf076d8d51dbf3fb7379d445b7dd7d96a44d43
Kristinn Björgvinsson <kristinnb17@ru.is>
2019-12-13 19:25:04 +0000

    Time juice issue fixed

--- Horology/Assets/CameraMovementController.cs
+++ Horology/Assets/CameraMovementController.cs
@@ -7,11 +7,12 @@ public class CameraMovementController : MonoBehaviour
 
     // The object the camera follows
     public GameObject followObject;
+    public float followTime;
 
     // Update is called once per frame
     void Update()
     {
         // Follow 20% of the way to the player
-        transform.position += (followObject.transform.position - new Vector3(transform.position.x, transform.position.y, followObject.transform.position.z)) * 0.07f;
+        transform.position += (followObject.transform.position - new Vector3(transform.position.x, transform.position.y, followObject.transform.position.z)) * (Time.deltaTime * followTime);
     }
 }
